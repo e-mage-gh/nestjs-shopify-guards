c628f457cb65010a1f56676020a70939cc940aa0
Tomasz Zwierzchon <t.zwierzchon@e-mage.pl>
2023-01-27 10:43:11 +0100

    fix: Remove unused configuration variables NGHFS-2

--- lib/interfaces/shopify-guards-module-options.interface.ts
+++ lib/interfaces/shopify-guards-module-options.interface.ts
@@ -1,8 +1,5 @@
 export interface ShopifyGuardsModuleOptions {
   apiSecretKey: string;
   headerHmac?: string;
-  headerShopDomain?: string;
-  headerRequestId?: string;
-  headerApiVersion?: string;
   queryHmac?: string;
 }
